d2e0d062cc97991201a1af7c7055cf5263acf9e8
wildes <portalesweb@abakosoluciones.com>
2024-07-15 18:32:40 -0500

    feature(character): tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dragonBallWild';
 }
---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.css',
+})
+export class ToolbarComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+}
