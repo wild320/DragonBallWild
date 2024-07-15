c76c675eaaf8bbbc0c98566be5eae7fc0c8a2ca1
wildes <portalesweb@abakosoluciones.com>
2024-07-15 18:32:20 -0500

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CharactersComponent } from './characters/characters.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },  
+  {
+    path: 'characters',
+    component: CharactersComponent
+  }
+];
