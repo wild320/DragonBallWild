c320f62cf4e1c1dec1d3eeecffd7c607cee543e9
wild320 <wildesjaramillo@gmail.com>
2026-03-01 23:24:56 -0500

    build(seo): implement lazy-loading routing architecture and professional SEO metadata tags

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,26 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { CharactersComponent } from './characters/characters.component';
-import { BatallasComponent } from './batallas/batallas.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
-  { path: 'welcome', component: HomeComponent },
-  { path: 'characters', component: CharactersComponent },
-  { path: 'batallas', component: BatallasComponent },
+  {
+    path: 'welcome',
+    loadComponent: () =>
+      import('./home/home.component').then((c) => c.HomeComponent),
+    title: 'Dragon Ball Wild | Home',
+  },
+  {
+    path: 'characters',
+    loadComponent: () =>
+      import('./characters/characters.component').then(
+        (c) => c.CharactersComponent,
+      ),
+    title: 'Dragon Ball Wild | Enciclopedia',
+  },
+  {
+    path: 'batallas',
+    loadComponent: () =>
+      import('./batallas/batallas.component').then((c) => c.BatallasComponent),
+    title: 'Dragon Ball Wild | Batallas épicas',
+  },
+  { path: '**', redirectTo: '/welcome' },
 ];
